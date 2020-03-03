c95fc0b205c78fe563b433f8886f208e34d3244b
jvpersuhn <bljoviph@gmail.com>
2020-03-02 23:19:11 -0300

    Exercicios

--- MaxCounters.py
+++ MaxCounters.py
@@ -1,15 +1,22 @@
 def solution(N, A):
     l = [0]*N
-
     m = 0
+    m2 = 0
     for i in A:
-        if i < N + 1:
+        if i <= N:
+            if m > l[i - 1]:
+                l[i - 1] = m
             l[i - 1] += 1
-            if l[i - 1] > m:
-                m = l[i - 1]
+            if m2 < l[i - 1]:
+                m2 = l[i - 1]
         else:
-            l = [m] * N
+            m = m2
+
+    for i in range(0, N):
+        if l[i] < m:
+            l[i] = m
     return l
 
 l = [3,4,4,6,1,4,4]
 print(solution(5, l))
+
